bce016e6fc0079c837e567e2383f1c4c4e78a8fb
Marcos Docampo <marcosdo@ucm.es>
2022-12-03 14:12:25 +0100

    Practica 2.3 - Ejercicios 8-11
    Siguientes ejercicios de la entrega de la práctica

---
+++ practica2.3/ejercicio9.c
@@ -0,0 +1,15 @@
+/*
+NAME
+       kill - terminate a process
+
+SYNOPSIS
+       kill [-s signal|-p] [-q sigval] [-a] [--] pid...
+       kill -l [signal]
+
+DESCRIPTION
+       The command kill sends the specified signal to the specified process or process group. 
+
+       -p, --pid
+              Specify that kill should only print the process id (pid) of the named  processes,  and
+              not send any signals.
+*/
